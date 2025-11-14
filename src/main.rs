10e1adc258010a0fe73980eaf652de7888602df1
João Paulo Abdala Bohaczk <okiabdala@gmail.com>
2025-11-14 10:29:15 -0300

    using flags and added readme with tutorial

--- src/main.rs
+++ src/main.rs
@@ -34,19 +34,45 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
 impl Config {
     fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() > 1 && (args[1] == "-h" || args[1] == "--help") {
+            Config::return_help_text();
+        }
         if args.len() < 3 {
             return Err("not enought arguments");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        let mut ignore_case = false;
+
+        for arg in args.iter().skip(3) {
+            match arg.as_str() {
+                "-i" | "--ignore-case" => {
+                    ignore_case = true;
+                }
+                "-h" | "--help" => {
+                    Config::return_help_text();
+                }
+                _ => {
+                    eprintln!("Try 'grep --help' for more information");
+                    process::exit(0);
+                }
+            }
+        }
 
         Ok(Config {
             query,
             file_path,
             ignore_case,
         })
+    }
+}
+
+impl Config {
+    fn return_help_text() {
+        println!("Usage: minigrep <QUERY> <FILE_PATH> [OPTIONS]");
+        println!("Options: -i/--ignore-case, -h/--help");
+        process::exit(0);
     }
 }
 
